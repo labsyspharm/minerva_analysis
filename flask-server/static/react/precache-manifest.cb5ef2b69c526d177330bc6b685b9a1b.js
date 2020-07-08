b6f7d5ad2c6d8b3f90c7b48c275c46b0c2fdd9f4
Simon Warchol <me@simonwarchol.com>
2020-07-08 19:35:20 -0400

    boilerplate react/flask

---
+++ flask-server/static/react/precache-manifest.cb5ef2b69c526d177330bc6b685b9a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc2d7f9bf2e839812009453ed394cb8",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "7227e19726c2badb8ced",
+    "url": "/static/react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "56f75155cd0c17861a70",
+    "url": "/static/react/static/js/2.7df45a69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/static/js/2.7df45a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7227e19726c2badb8ced",
+    "url": "/static/react/static/js/main.7d7196ca.chunk.js"
+  },
+  {
+    "revision": "93e9bd82758163cbfb7e",
+    "url": "/static/react/static/js/runtime-main.87943426.js"
+  }
+]);
---
+++ flask-server/static/react/static/js/main.7d7196ca.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-app"]=this["webpackJsonpreact-app"]||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),c=t(2),o=t.n(c);t(8);var p=function(){return r.a.createElement("p",null,"My Token = ",window.token)};o.a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(p,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.7d7196ca.chunk.js.map
