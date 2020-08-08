b7c36922d9a978cfaa1b78b10233b53a5f6ee859
Simon Warchol <me@simonwarchol.com>
2020-08-08 15:54:51 -0400

    adding neighborhood array

---
+++ server/save_neighborhood_matrix.py
@@ -0,0 +1,37 @@
+from sklearn.neighbors import BallTree
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from joblib import Parallel, delayed
+import multiprocessing
+
+import umap
+from numba import njit
+
+num_cores = multiprocessing.cpu_count()
+raw_data = pd.read_csv("../static/data/tonsil_with_phenotype.csv")
+data = raw_data[['X_centroid', 'Y_centroid', 'phenotype']].copy()
+phenotypes = {val: idx for idx, val in enumerate(data.phenotype.unique())}
+points = pd.DataFrame({'x': raw_data['X_centroid'], 'y': raw_data['Y_centroid']})
+ball_tree = BallTree(points, metric='euclidean')
+temp = []
+neighborhood_matrix = np.zeros((data.shape[0], len(phenotypes)))
+A = data.to_numpy()
+n = []
+d = []
+for i in tqdm(range(A.shape[0])):
+    index, distance = ball_tree.query_radius([[A[i, 0], A[i, 1]]], r=50, return_distance=True)
+    neighbors = list(index[0])
+    distances = list(distance[0])
+    for k in range(len(neighbors)):
+        neighbor = neighbors[k]
+        distance = distances[k]
+        neighbor_phenotype = A[neighbor,2]
+        neighbor_phenotype_index = phenotypes[neighbor_phenotype]
+        weight = 1
+        if distance > 25:
+            weight = 1 - (distance - 25) / 25
+        neighborhood_matrix[i, neighbor_phenotype_index] += weight
+
+np.save('../static/data/neighborhood_array.npy', neighborhood_matrix)
+
---
+++ server/save_umap_euclidean.py
@@ -0,0 +1,21 @@
+from sklearn.neighbors import BallTree
+from tqdm import tqdm
+import numpy as np
+import umap
+import time
+
+metric = 'euclidean'
+A = np.load('../static/data/neighborhood_array.npy')
+for n_neighbors in [10, 100, 200]:
+    for min_dist in [.2, .8]:
+        start_time = time.time()
+        save_string = 'full_weighted_umap_' + metric + '_' + str(n_neighbors) + 'neighbors_' + str(min_dist) + 'dist'
+        save_string = save_string.replace('.', '')
+        print(save_string)
+        if (os.path.isfile(save_string + '.npy')):
+            print("skipping")
+        else:
+            new_data_embedding = umap.UMAP(metric=metric, n_neighbors=n_neighbors, min_dist=min_dist).fit_transform(
+                A)
+            np.save(save_string, new_data_embedding)
+        print("--- %s seconds ---" % (time.time() - start_time))
