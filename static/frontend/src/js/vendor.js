763f8152619f56b7828cc372de9018cf403846a2
Simon Warchol <me@simonwarchol.com>
2020-08-19 16:57:54 -0400

    moving things around and adding roberts plot

--- static/frontend/src/js/vendor.js
+++ static/frontend/src/js/vendor.js
@@ -1,25 +1,29 @@
 import {Buffer} from 'buffer/';
-import {PNG} from 'pngjs'
-import 'popper.js'
-import 'jquery'
-import 'bootstrap'
+import {PNG} from 'pngjs';
+import 'popper.js';
+import 'jquery';
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'pngjs'
+import 'pngjs';
 import * as d3 from 'd3';
 import * as slider from 'd3-simple-slider';
-import 'lodash'
-import 'jquery-form'
-import '@fortawesome/fontawesome-free/js/all'
+import {legendColor} from 'd3-svg-legend'
+import colorbrewer from 'colorbrewer';
+import 'lodash';
+import 'jquery-form';
+import '@fortawesome/fontawesome-free/js/all';
 import Sortable from 'sortablejs';
-import Mark from 'mark.js'
-import $ from 'jquery'
-import 'node-fetch'
-import convert from 'color-convert'
+import Mark from 'mark.js';
+import $ from 'jquery';
+import 'node-fetch';
+import convert from 'color-convert';
 
 window.convert = convert;
 window.$ = $;
 window.d3 = d3;
 window.d3.slider = slider;
+window.legendColor = legendColor;
+window.colorbrewer = colorbrewer;
 window.PNG = PNG;
 window.Buffer = Buffer;
 window.Sortable = Sortable;
