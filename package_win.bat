7a82c3f97d3f1f800c2c617846e15bb3d647c7da
Simon Warchol <me@simonwarchol.com>
2021-01-16 00:34:13 -0500

    adding icon

--- package_win.bat
+++ package_win.bat
@@ -1,1 +1,1 @@
-pyinstaller -F --paths $env:CONDA_PREFIX --add-data "cycif_viewer;cycif_viewer" --add-data "%CONDA_PREFIX%/Lib/site-packages/xmlschema/schemas;xmlschema/schemas" --hidden-import "scipy.spatial.transform._rotation_groups" --hidden-import cmath --name cycif_viewer run.py
+pyinstaller -F --paths $env:CONDA_PREFIX --add-data "cycif_viewer;cycif_viewer" --add-data "%CONDA_PREFIX%/Lib/site-packages/xmlschema/schemas;xmlschema/schemas" --hidden-import "scipy.spatial.transform._rotation_groups" --hidden-import cmath --icon icon.ico  --name cycif_viewer run.py
