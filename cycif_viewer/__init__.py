4b0bf14eac21fc438cb021131a3426b2703d1a86
Simon Warchol <me@simonwarchol.com>
2021-01-18 11:15:17 -0500

    moving templates to client

--- cycif_viewer/__init__.py
+++ cycif_viewer/__init__.py
@@ -11,9 +11,7 @@ import json
 import sys
 import multiprocessing
 
-
-
-app = Flask(__name__, template_folder=Path('server/templates'), static_folder='data')
+app = Flask(__name__, template_folder=Path('client/templates'), static_folder='data')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///server/db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
