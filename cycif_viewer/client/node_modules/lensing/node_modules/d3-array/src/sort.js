a3bbddbe7177afeced8cf59f956a008c1b8f6989
Simon Warchol <me@simonwarchol.com>
2021-01-25 11:11:27 -0500

    removing node_modules

--- cycif_viewer/client/node_modules/lensing/node_modules/d3-array/src/sort.js
+++
@@ -1,6 +0,0 @@
-import ascending from "./ascending.js";
-
-export default function sort(values, comparator = ascending) {
-  if (typeof values[Symbol.iterator] !== "function") throw new TypeError("values is not iterable");
-  return Array.from(values).sort(comparator);
-}
