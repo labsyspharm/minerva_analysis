f1e5f31372cc97ce68bc928461c9eef1d41a3cf9
kruegert <krueger@g.harvard.edu>
2021-04-29 09:05:52 -0400

    renaming phenotype column to celltype

--- cycif_viewer/server/models/data_model.py
+++ cycif_viewer/server/models/data_model.py
@@ -9,11 +9,14 @@ from ome_types import from_xml
 from cycif_viewer import config_json_path, data_path
 from cycif_viewer.server.utils import pyramid_assemble
 from cycif_viewer.server.models import database_model
+import dateutil.parser
 import time
 import pickle
 import tifffile as tf
 import re
 import zarr
+from dask import dataframe as dd
+import cv2
 
 ball_tree = None
 database = None
@@ -41,22 +44,26 @@ def load_datasource(datasource_name, reload=False):
     if reload:
         load_ball_tree(datasource_name, reload=reload)
     csvPath = Path(config[datasource_name]['featureData'][0]['src'])
+    print("Loading csv data.. (this can take some time)")
     datasource = pd.read_csv(csvPath)
     datasource['id'] = datasource.index
     datasource = datasource.replace(-np.Inf, 0)
     source = datasource_name
+    print("Loading segmentation.")
     if config[datasource_name]['segmentation'].endswith('.zarr'):
         seg = zarr.load(config[datasource_name]['segmentation'])
     else:
         seg_io = tf.TiffFile(config[datasource_name]['segmentation'], is_ome=False)
         seg = zarr.open(seg_io.series[0].aszarr())
     channel_io = tf.TiffFile(config[datasource_name]['channelFile'], is_ome=False)
+    print("Loading image descriptions.")
     try:
         xml = channel_io.pages[0].tags['ImageDescription'].value
         metadata = from_xml(xml).images[0].pixels
     except:
         metadata = {}
     channels = zarr.open(channel_io.series[0].aszarr())
+    print("Data loading done.")
 
 
 def load_config(datasource_name):
@@ -102,8 +109,9 @@ def load_ball_tree(datasource_name_name, reload=False):
     if os.path.isfile(pickled_kd_tree_path) and reload is False:
         print("Pickled KD Tree Exists, Loading")
         ball_tree = pickle.load(open(pickled_kd_tree_path, "rb"))
+        print("Pickled KD Tree Loaded.")
     else:
-        print("Creating KD Tree")
+        print("Creating KD Tree.")
         xCoordinate = config[datasource_name_name]['featureData'][0]['xCoordinate']
         yCoordinate = config[datasource_name_name]['featureData'][0]['yCoordinate']
         csvPath = Path(config[datasource_name_name]['featureData'][0]['src'])
@@ -111,6 +119,7 @@ def load_ball_tree(datasource_name_name, reload=False):
         points = pd.DataFrame({'x': raw_data[xCoordinate], 'y': raw_data[yCoordinate]})
         ball_tree = BallTree(points, metric='euclidean')
         pickle.dump(ball_tree, open(pickled_kd_tree_path, 'wb'))
+        print('Creating KD Tree done.')
 
 
 def query_for_closest_cell(x, y, datasource_name):
@@ -127,8 +136,8 @@ def query_for_closest_cell(x, y, datasource_name):
         try:
             row = datasource.iloc[index[0]]
             obj = row.to_dict(orient='records')[0]
-            if 'phenotype' not in obj:
-                obj['phenotype'] = ''
+            if 'celltype' not in obj:
+                obj['celltype'] = ''
             return obj
         except:
             return {}
@@ -179,16 +188,62 @@ def get_channel_cells(datasource_name, channels):
     return query
 
 
+def get_phenotype_description(datasource):
+    try:
+        data = ''
+        csvPath = config[datasource]['featureData'][0]['celltypeData']
+        if os.path.isfile(csvPath):
+            data = pd.read_csv(csvPath)
+            data = data.to_numpy().tolist()
+            # data = data.to_json(orient='records', lines=True)
+        return data;
+    except KeyError:
+        return ''
+    except TypeError:
+        return ''
+
+
+def get_phenotype_column_name(datasource):
+    try:
+        return config[datasource]['featureData'][0]['celltype']
+    except KeyError:
+        return ''
+    except TypeError:
+        return ''
+
+
+def get_cells_phenotype(datasource_name):
+    global datasource
+    global source
+    global ball_tree
+
+    range = [0, 65536]
+
+    # Load if not loaded
+    if datasource_name != source:
+        load_ball_tree(datasource_name)
+
+    try:
+        phenotype_field = config[datasource_name]['featureData'][0]['celltype']
+    except KeyError:
+        phenotype_field = 'celltype'
+    except TypeError:
+        phenotype_field = 'celltype'
+
+    query = datasource[['id', phenotype_field]].to_dict(orient='records')
+    return query
+
+
 def get_phenotypes(datasource_name):
     global datasource
     global source
     global config
     try:
-        phenotype_field = config[datasource_name]['featureData'][0]['phenotype']
+        phenotype_field = config[datasource_name]['featureData'][0]['celltype']
     except KeyError:
-        phenotype_field = 'phenotype'
+        phenotype_field = 'celltype'
     except TypeError:
-        phenotype_field = 'phenotype'
+        phenotype_field = 'celltype'
 
     if datasource_name != source:
         load_ball_tree(datasource_name)
@@ -233,7 +288,7 @@ def get_number_of_cells_in_circle(x, y, datasource_name, r):
         return 0
 
 
-def get_color_scheme(datasource_name, refresh, label_field='phenotype'):
+def get_color_scheme(datasource_name, refresh, label_field='celltype'):
     color_scheme_path = str(
         Path(os.path.join(os.getcwd())) / data_path / datasource_name / str(
             label_field + "_color_scheme.pickle"))
@@ -242,11 +297,12 @@ def get_color_scheme(datasource_name, refresh, label_field='phenotype'):
             print("Color Scheme Exists, Loading")
             color_scheme = pickle.load(open(color_scheme_path, "rb"))
             return color_scheme
-    if label_field == 'phenotype':
+    if label_field == 'celltype':
         labels = get_phenotypes(datasource_name)
+        print(labels)
     labels.append('SelectedCluster')
     color_scheme = {}
-    colors = ["#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059", "#FFDBE5", "#7A4900",
+    colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#a65628", "#f781bf", "#808080", "#7A4900",
               "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87", "#5A0007", "#809693", "#FEFFE6",
               "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80", "#61615A", "#BA0900", "#6B7900", "#00C2A0",
               "#FFAA92", "#FF90C9", "#B903AA", "#D16100", "#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018",
@@ -303,8 +359,8 @@ def get_rect_cells(datasource_name, rect, channels):
         for neighbor in neighbors:
             row = datasource.iloc[[neighbor]]
             obj = row.to_dict(orient='records')[0]
-            if 'phenotype' not in obj:
-                obj['phenotype'] = ''
+            if 'celltype' not in obj:
+                obj['celltype'] = ''
             neighborhood.append(obj)
         return neighborhood
     except:
@@ -396,9 +452,12 @@ def get_datasource_description(datasource_name):
     # Load if not loaded
     if datasource_name != source:
         load_ball_tree(datasource_name)
-    description = datasource.describe().to_dict()
+    description = datasource.describe(percentiles=[.005, .01, .25, .5, .75, .95, .99, .995]).to_dict()
     for column in description:
-        [hist, bin_edges] = np.histogram(datasource[column].to_numpy(), bins=50, density=True)
+        col = datasource[column]
+        col = col[(col >= description[column]['1%']) & (col <= description[column]['99%'])]
+        col = col.to_numpy()
+        [hist, bin_edges] = np.histogram(col, bins=25, density=True)
         midpoints = (bin_edges[1:] + bin_edges[:-1]) / 2
         description[column]['histogram'] = {}
         dat = []
@@ -409,6 +468,95 @@ def get_datasource_description(datasource_name):
             dat.append(obj)
         description[column]['histogram'] = dat
     return description
+
+
+
+def spatial_corr (adata, raw=False, log=False, threshold=None, x_coordinate='X_centroid',y_coordinate='Y_centroid',
+                  marker=None, k=500, label='spatial_corr'):
+    """
+    Parameters
+    ----------
+    adata : TYPE
+        DESCRIPTION.
+    raw : TYPE, optional
+        DESCRIPTION. The default is False.
+    log : TYPE, optional
+        DESCRIPTION. The default is False.
+    threshold : TYPE, optional
+        DESCRIPTION. The default is None.
+    x_coordinate : TYPE, optional
+        DESCRIPTION. The default is 'X_centroid'.
+    y_coordinate : TYPE, optional
+        DESCRIPTION. The default is 'Y_centroid'.
+    marker : TYPE, optional
+        DESCRIPTION. The default is None.
+    k : TYPE, optional
+        DESCRIPTION. The default is 500.
+    label : TYPE, optional
+        DESCRIPTION. The default is 'spatial_corr'.
+    Returns
+    -------
+    corrfunc : TYPE
+        DESCRIPTION.
+    Example
+    -------
+    adata = spatial_corr (adata, threshold=0.5, x_coordinate='X_position',y_coordinate='Y_position',marker=None)
+    """
+    # Start
+    bdata = adata.copy()
+    # Create a DataFrame with the necessary inforamtion
+    data = pd.DataFrame({'x': bdata.obs[x_coordinate], 'y': bdata.obs[y_coordinate]})
+    # user defined expression matrix
+    if raw is True:
+        exp =  pd.DataFrame(bdata.raw.X, index= bdata.obs.index, columns=bdata.var.index)
+    else:
+        exp =  pd.DataFrame(bdata.X, index= bdata.obs.index, columns=bdata.var.index)
+    # log the data if needed
+    if log is True:
+        exp = np.log1p(exp)
+    # set a threshold if needed
+    if threshold is not None:
+        exp[exp >= threshold] = 1
+        exp[exp < threshold] = 0
+    # subset markers if needed
+    if marker is not None:
+        if isinstance(marker, str):
+            marker = [marker]
+        exp = exp[marker]
+    # find the nearest neighbours
+    tree = BallTree(data, leaf_size= 2)
+    dist, ind = tree.query(data, k=k, return_distance= True)
+    neighbours = pd.DataFrame(ind, index = bdata.obs.index)
+    # find the mean dist
+    rad_approx = np.mean(dist, axis=0)
+    # Calculate the correlation
+    mean = np.mean(exp).values
+    std = np.std(exp).values
+    A = (exp - mean) / std
+    def corrfunc (marker, A, neighbours, ind):
+        print('Processing ' + str(marker))
+        # Map phenotype
+        ind_values = dict(zip(list(range(len(ind))), A[marker])) # Used for mapping
+        # Loop through (all functionized methods were very slow)
+        neigh = neighbours.copy()
+        for i in neigh.columns:
+            neigh[i] = neigh[i].dropna().map(ind_values, na_action='ignore')
+        # multiply the matrices
+        Y = neigh.T * A[marker]
+        corrfunc = np.mean(Y, axis=1)
+        # return
+        return corrfunc
+    # apply function to all markers    # Create lamda function
+    r_corrfunc = lambda x: corrfunc(marker=x,A=A, neighbours=neighbours, ind=ind)
+    all_data = list(map(r_corrfunc, exp.columns)) # Apply function
+    # Merge all the results into a single dataframe
+    df = pd.concat(all_data, axis=1)
+    df.columns = exp.columns
+    df['distance'] = rad_approx
+    # add it to anndata object
+    adata.uns[label] = df
+    # return
+    return adata
 
 
 def generate_zarr_png(datasource_name, channel, level, tile):
@@ -489,3 +637,22 @@ def convertOmeTiff(filePath, channelFilePath=None, dataDirectory=None, isLabelIm
         pyramid_assemble.main(py_args=args)
 
         return {'segmentation': str(directory)}
+
+
+def save_dot(datasource_name, dot):
+    database_model.create_or_update(database_model.Dot, id=dot['id'], datasource=datasource_name, group=dot['group'],
+                                    name=dot['name'], description=dot['description'], shape_type=dot['shape_type'],
+                                    shape_info=dot['shape_info'], cell_ids=dot['cell_ids'],
+                                    date=dateutil.parser.parse(dot['date']), image_data=dot['image_data'],
+                                    viewer_info=dot['viewer_info'], channel_info=dot['channel_info'])
+
+
+def load_dots(datasource_name):
+    dots = database_model.get_all(database_model.Dot, datasource=datasource_name)
+    print(dots)
+    return dots
+
+
+def delete_dot(datasource_name, id):
+    database_model.edit(database_model.Dot, id, 'is_deleted', True)
+    return True
