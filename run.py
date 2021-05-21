18d4c7d5814c503c7ca71fab221e3a095316573f
Simon Warchol <me@simonwarchol.com>
2021-05-21 07:16:50 -0500

    Adding loging when server spins up

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ import sys
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
+    print("Server Running")
     serve(app, host='0.0.0.0', port=8000, max_request_body_size=107374182400, max_request_header_size=8589934592)
