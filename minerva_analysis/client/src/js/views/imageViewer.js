a02e7709bc7179db02a3109864f2cca13eb23dc5
Simon Warchol <simonwarchol@g.harvard.edu>
2021-09-14 09:47:02 -0400

    Attempting to fix issue with seg tiles being redrawn

--- minerva_analysis/client/src/js/views/imageViewer.js
+++ minerva_analysis/client/src/js/views/imageViewer.js
@@ -110,7 +110,6 @@ class ImageViewer {
         //
         seaGL.addHandler('tile-drawing', async function (callback, e) {
 
-
             // Read parameters from each tile
             const tile = e.tile;
             const group = e.tile.url.split("/");
@@ -140,10 +139,19 @@ class ImageViewer {
                 // await that.drawLabels(e);
             } else {
                 if (e.tile._redrawLabel) {
+                    if (!e.tile._array || !e.tile._tileImageData) {
+                        console.log('Missing Array', e.tile.url);
+                        // this.refreshSegmentationMask();
+                    }
                     that.drawLabelTile(e.tile, e.tile._tileImageData.width, e.tile._tileImageData.height);
                 }
                 if (e.tile.containsLabel) {
-                    e.rendered.putImageData(e.tile._tileImageData, 0, 0);
+                    try {
+                        e.rendered.putImageData(e.tile._tileImageData, 0, 0);
+                    } catch (err) {
+                        console.log('Another issue', err, e.tile.url);
+                        // this.refreshSegmentationMask();
+                    }
                 }
             }
         });
@@ -172,45 +180,50 @@ class ImageViewer {
 
 
         seaGL.addHandler('tile-loaded', (callback, e) => {
-            try {
-                const group = e.tile.url.split("/");
-                let isLabel = group[group.length - 3] == that.labelChannel.sub_url;
-                e.tile._blobUrl = e.image.src;
-                // Label Tiles We'll view as 32 bits to get the ID values and save that on the tile object so it's cached
-                if (isLabel) {
-                    e.tile._array = new Int32Array(PNG.sync.read(new Buffer(e.tileRequest.response), {colortype: 0}).data.buffer);
+            var decoder = new Promise(function (resolve, reject) {
+                try {
+                    const group = e.tile.url.split("/");
+                    let isLabel = group[group.length - 3] == that.labelChannel.sub_url;
+                    e.tile._blobUrl = e.image?.src;
+                    if (isLabel) {
+                        e.tile._isLabel = true;
+                        if (!e.tile._array) {
+                            e.tile._array = new Int32Array(PNG.sync.read(new Buffer(e.tileRequest?.response ||
+                                e.image._array), {colortype: 0}).data.buffer);
+                        }
+                        that.drawLabelTile(e.tile, e.image?.width || e.tile?._tileImageData?.width, e.image?.height
+                            || e.tile?._tileImageData?.height);
 
-                    that.drawLabelTile(e.tile, e.image.width, e.image.height);
+                        // We're hence skipping that OpenseadragonGL callback since we only care about the vales
+                        return resolve();
+                    } else {
+                        return callback(e)
+                        // This goes to OpenseadragonGL which does the necessary bit stuff.
+                    }
+                } catch (err) {
+                    console.log('Load Error, Refreshing', err, e.tile.url);
+                    that.forceRepaint();
 
-                    // We're hence skipping that OpenseadragonGL callback since we only care about the vales
-                    return e.getCompletionCallback()();
-                } else {
-                    // This goes to OpenseadragonGL which does the necessary bit stuff.
-                    return callback(e);
+                    // return callback(e);
                 }
-            } catch (e) {
-                console.log('Load Error, Refreshing');
-                that.forceRepaint();
-
-                // return callback(e);
-            }
+                // Notify openseadragon when decoded
+                decoder.then(e.getCompletionCallback())
+            });
         });
-
 
 
         this.viewer.addHandler('tile-drawn', (e) => {
             let count = _.size(e.tiledImage._tileCache._tilesLoaded);
             e.tiledImage._tileCache._imagesLoadedCount = count;
-
         })
 
         this.viewer.addHandler('tile-unloaded', (e) => {
-            (window.URL || window.webkitURL).revokeObjectURL(e.tile._blobUrl);
+            if (e.tile._blobUrl) {
+                (window.URL || window.webkitURL).revokeObjectURL(e.tile._blobUrl);
+            }
             delete e.tile._array;
             delete e.tile._tileImageData;
         })
-
-
 
 
         // Instantiate viewer managers
@@ -595,9 +608,6 @@ ImageViewer
 
 async function addTile(path) {
     const addJob = new Promise((resolve, reject) => {
-        if (seaDragonViewer.tileCache[path]) {
-            resolve();
-        }
 
         // If we're currently waiting for a tile to load, just use it's callback
         if (seaDragonViewer.pendingTiles.has(path)) {
