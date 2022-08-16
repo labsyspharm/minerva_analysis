0591b53f6bedce1f25739b135b8c9301112d27d7
kruegert <krueger@g.harvard.edu>
2022-08-16 17:33:15 -0400

    added option to specify port via command line

--- run.py
+++ run.py
@@ -4,5 +4,7 @@ import multiprocessing
 import sys
 
 if __name__ == '__main__':
-    print('Serving on 0.0.0.0:8000 or http://localhost:8000/')
-    serve(app, host='0.0.0.0', port=8000, max_request_body_size=1073741824000000, max_request_header_size=85899345920000)
+    #use port 8000 if no port is specified via command line argument
+    port = 8000 if len(sys.argv) < 2 or not str.isdigit(sys.argv[1]) else sys.argv[1]
+    print('Serving on 0.0.0.0:' + str(port) + ' or http://localhost:' + str(port) )
+    serve(app, host='0.0.0.0', port=port, max_request_body_size=1073741824000000, max_request_header_size=85899345920000)
