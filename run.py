637924b3a50cb741eb8b432dd7f97e6f9149e0d6
kruegert <krueger@g.harvard.edu>
2022-08-16 12:52:18 -0400

    updated run.py to enable port specification via command line.

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ import multiprocessing
 import sys
 
 if __name__ == '__main__':
+    print (len(sys.argv))
     multiprocessing.freeze_support()
     #use port 8000 if no port is specified via command line argument
-    port = 8000 if not sys.argv[1] or not isinstance(sys.argv[1], int) else sys.argv[1]
-
+    port = 8000 if len(sys.argv) < 2 or not str.isdigit(sys.argv[1]) else sys.argv[1]
     print('Serving on 0.0.0.0:' + str(port) + ' or http://localhost:' + str(port) )
     serve(app, host='0.0.0.0', port=port, max_request_body_size=107374182400, max_request_header_size=8589934592)
 
