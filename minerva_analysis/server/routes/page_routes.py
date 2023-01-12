b1822cfad7c702ab9f98c4857fba38d446ae2808
Simon Warchol <simonwarchol@g.harvard.edu>
2023-01-12 14:19:18 -0500

    Adding docker support

--- minerva_analysis/server/routes/page_routes.py
+++ minerva_analysis/server/routes/page_routes.py
@@ -7,7 +7,8 @@ import os
 
 @app.route("/")
 def my_index():
-    return render_template("index.html", data={'datasource': '', 'datasources': get_config_names()})
+    return render_template("index.html", data={'datasource': '', 'datasources': get_config_names(),
+                                               'is_docker': app.config['IS_DOCKER']})
 
 
 @app.route('/<string:datasource>')
@@ -15,13 +16,15 @@ def image_viewer(datasource):
     datasources = get_config_names()
     if datasource not in datasources:
         datasource = ''
-    return render_template('index.html', data={'datasource': datasource, 'datasources': datasources})
+    return render_template('index.html', data={'datasource': datasource, 'datasources': datasources,
+                                               'is_docker': app.config['IS_DOCKER']})
 
 
 
 @app.route("/upload_page")
 def upload_page():
-    return render_template("upload.html", data={'datasource': '', 'datasources': get_config_names()})
+    return render_template("upload.html", data={'datasource': '', 'datasources': get_config_names(),
+                                                'is_docker': app.config['IS_DOCKER']})
 
 
 
