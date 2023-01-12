9d7bd9854203c561687b29c9116d268b18c00a75
Simon Warchol <simonwarchol@g.harvard.edu>
2023-01-12 14:14:39 -0500

    Adding new run

--- run.py
+++ run.py
@@ -4,9 +4,18 @@ import multiprocessing
 import sys
 
 if __name__ == '__main__':
-    multiprocessing.freeze_support()
     #use port 8000 if no port is specified via command line argument
     port = 8000 if len(sys.argv) < 2 or not str.isdigit(sys.argv[1]) else sys.argv[1]
-    print('Serving on 0.0.0.0:' + str(port) + ' or http://localhost:' + str(port) )
-    serve(app, host='0.0.0.0', port=port, max_request_body_size=107374182400, max_request_header_size=8589934592)
 
+
+    def str2bool(v):
+        return v.lower() in ("yes", "true", "t", "1")
+    if len(sys.argv) > 2 or not str2bool(sys.argv[2]):
+        is_docker = True
+    else:
+        is_docker = False
+    app.config['IS_DOCKER'] = is_docker
+
+
+    print('Serving on 0.0.0.0:' + str(port) + ' or http://localhost:' + str(port) )
+    serve(app, host='0.0.0.0', port=port, max_request_body_size=1073741824000000, max_request_header_size=85899345920000)
