fed8232cab6d9f8c23f1447a19aee1d3c311509f
Simon Warchol <simonwarchol@g.harvard.edu>
2023-01-13 10:22:52 -0500

    Fixing run script

--- run.py
+++ run.py
@@ -1,21 +1,24 @@
-from waitress import serve
-from minerva_analysis import app
-import multiprocessing
 import sys
 
+from waitress import serve
+
+from minerva_analysis import app
+
 if __name__ == '__main__':
-    #use port 8000 if no port is specified via command line argument
+    # use port 8000 if no port is specified via command line argument
     port = 8000 if len(sys.argv) < 2 or not str.isdigit(sys.argv[1]) else sys.argv[1]
 
 
     def str2bool(v):
         return v.lower() in ("yes", "true", "t", "1")
-    if len(sys.argv) > 2 or not str2bool(sys.argv[2]):
+
+
+    if len(sys.argv) > 2 and str2bool(sys.argv[2]):
         is_docker = True
     else:
         is_docker = False
     app.config['IS_DOCKER'] = is_docker
 
-
-    print('Serving on 0.0.0.0:' + str(port) + ' or http://localhost:' + str(port) )
-    serve(app, host='0.0.0.0', port=port, max_request_body_size=1073741824000000, max_request_header_size=85899345920000)
+    print('Serving on 0.0.0.0:' + str(port) + ' or http://localhost:' + str(port))
+    serve(app, host='0.0.0.0', port=port, max_request_body_size=1073741824000000,
+          max_request_header_size=85899345920000)
